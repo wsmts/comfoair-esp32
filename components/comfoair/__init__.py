86a4fb3caa1c9e6caf3b0db509063e7e47683472
Wim Smets <59442406+wsmts@users.noreply.github.com>
2024-03-06 22:05:22 +0100

    Update __init__.py
    changed git for esp32can

--- components/comfoair/__init__.py
+++ components/comfoair/__init__.py
@@ -135,8 +135,8 @@ CONFIG_SCHEMA = cv.All(
 
 async def to_code(config):
     cg.add_library("SPI", "2.0.0")
-    cg.add_library("can_common", None, "https://github.com/collin80/can_common.git#07605a2a9f4963ee68a9ecf7790d38b22f6d2cdf")
-    cg.add_library("esp32_can", None, "https://github.com/collin80/esp32_can.git#0fb9878a77935d2995e094d196971009e0da3c48")
+    cg.add_library("can_common", None, "https://github.com/collin80/can_common.git")
+    cg.add_library("esp32_can", None, "https://github.com/collin80/esp32_can.git")
 
 
     var = cg.new_Pvariable(config[CONF_ID])
